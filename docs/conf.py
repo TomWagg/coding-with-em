a2b5682b3253263fc704a7bf1d8f65dc9f92d0da
Tom Wagg <tomjwagg@gmail.com>
2025-10-17 23:23:07 -0400

    add copy buttons

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@ author = 'Tom Wagg'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_copybutton',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
